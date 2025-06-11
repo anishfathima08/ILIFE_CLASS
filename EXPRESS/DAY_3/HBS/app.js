02b1ade36b0ba15ab7d8fed89ec9cc14859e245d
anishfathima08 <anisfathima324@gmail.com>
2025-06-11 18:49:48 +0530

    commit

--- EXPRESS/DAY_3/HBS/app.js
+++ EXPRESS/DAY_3/HBS/app.js
@@ -1,19 +1,43 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+// HBS - Handle Bars 
 
-app.set('view engine', 'hbs');
+// const express = require('express')
+// const app = express()
+// const PORT = 8080
 
-app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs')
+
+// app.get('/', (req, res) => {
+//   res.render('index', { title : 'Home', message : 'Welcome To Home Page' })
+// }) 
+
+// app.get('/about', (req, res) => {
+//   // res.render('about', { title : 'About', message : 'Welcome to About Page' })
+
+//   var title = 'About' 
+//   var message = 'Welcome to About Page'
+
+//   res.render('about', { title, message })
+
+// })
+
+// app.listen(PORT) 
+
+
+const express = require('express')
+const app = express()
+const PORT = 8080
+const path = require('path')
+
+app.set('view engine', 'hbs')
+
+app.set('views', path.join(__dirname, 'public'))
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Home Page', name: 'User' });
-});
+  res.render('index', { title : 'Home', message : 'Welcome To Home Page' })
+})
 
 app.get('/about', (req, res) => {
-  res.render('about', { title: 'About Page' });
-});
+  res.render('about', { title : 'About', message : 'Welcome To About Page' })
+})
 
-app.listen(8080, () => {
-  console.log('Server running on http://localhost:8080');
-});
+app.listen(PORT)
