9d390096d95006fe78c103362f1d26f92fe8b868
AnishFathimaS <anisfathi0808@gmail.com>
2025-07-11 06:53:19 +0530

    commit

--- FULL_STACK_NOTES/FRONTEND/REACT/my_app/src/Crud.jsx
+++ FULL_STACK_NOTES/FRONTEND/REACT/my_app/src/Crud.jsx
@@ -1,120 +1,144 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Crud = () => {
+  var [input, setInput] = useState("");
+  var [list, setList] = useState([]);
 
-        var [ input, setInput ] = useState('')
-        var [ list, setList ] = useState([])
+  var [viewText, setViewText] = useState("");
 
-        var [ viewText, setViewText ] = useState('')
+  var [edit, setEdit] = useState("");
+  var [editText, setEditText] = useState("");
 
-        var [ edit, setEdit ] = useState('')
-        var [ editText, setEditText ] = useState('')
+  const inputFun = (e) => {
+    setInput(e.target.value);
+  };
 
-        const inputFun = (e) => {
-                setInput(e.target.value)
-        }
+  const addFun = () => {
+    setList(list.concat(input));
+    setInput("");
+  };
 
-        const addFun = () => {
-                setList(list.concat(input))
-                setInput('')
-        }
+  const viewFun = (value) => {
+    setViewText(value);
+  };
 
-        const viewFun = (value) => {
-                setViewText(value)
-        }
+  const deleteFun = (index) => {
+    if (confirm("Are You Sure Want to Delete ?")) {
+      setList(list.filter((value, i) => index !== i));
+    }
+  };
 
-        const deleteFun = (index) => {
-                if(confirm('Are You Sure Want to Delete ?')){
-                        setList(list.filter((value, i) => index !== i))
-                }
-        }
+  const editFun = (index) => {
+    setEdit(index);
+    setEditText(list[index]);
+  };
 
-        const editFun = (index) => {
-                setEdit(index)
-                setEditText(list[index])
-        }
+  const editTextFun = (e) => {
+    setEditText(e.target.value);
+  };
 
-        const editTextFun = (e) => {
-                setEditText(e.target.value)
-        }
+  const updateFun = () => {
+    setList(list.map((value, index) => (edit === index ? editText : value)));
+    setEdit("");
+  };
 
-        const updateFun = () => {
-                setList(list.map((value, index) => edit === index ? editText : value ))
-                setEdit('')
-        }
+  return (
+    <>
+      <div className="container p-5">
+        <h3>To Do List</h3>
+        <div className="input-group">
+          <input
+            type="text"
+            className="form-control"
+            value={input}
+            onChange={inputFun}
+          />
+          <button className="btn btn-primary" onClick={addFun}>
+            Add
+          </button>
+        </div>
 
-        return (
-        <>
-                <div className="container p-5">
-                        <h3>To Do List</h3>
-                        <div className="input-group">
-                                <input type="text" className='form-control' value={input} onChange={inputFun}/>
-                                <button className='btn btn-primary' onClick={addFun}>Add</button>
-                        </div>
-                        
-                        {
-                                list.length === 0 
-                                ? 
-                                <p className='text-center text-danger my-5'>No Tasks</p>
-                                :
-                                <table className='table table-bordered my-5'>
-                                <thead>
-                                        <tr>
-                                                <th>List</th>
-                                                <th>Actions</th>
-                                        </tr>
-                                </thead>
-                                <tbody>
-                                        {
-                                                list.map((value, index) => (
-                                                        <tr key={index}>
-                                                                <td>
-                                                                        {
-                                                                                edit === index 
-                                                                                ? <input type="text" value={editText} onChange={editTextFun} className='form-control' />
-                                                                                : value
-                                                                        }       
+        {list.length === 0 ? (
+          <p className="text-center text-danger my-5">No Tasks</p>
+        ) : (
+          <table className="table table-bordered my-5">
+            <thead>
+              <tr>
+                <th>List</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {list.map((value, index) => (
+                <tr key={index}>
+                  <td>
+                    {edit === index ? (
+                      <input
+                        type="text"
+                        value={editText}
+                        onChange={editTextFun}
+                        className="form-control"
+                      />
+                    ) : (
+                      value
+                    )}
+                  </td>
+                  <td className="d-flex gap-5">
+                    <button
+                      className="flex-grow-1 btn btn-success"
+                      data-bs-toggle="modal"
+                      data-bs-target="#myModal"
+                      onClick={() => viewFun(value)}
+                    >
+                      View
+                    </button>
 
-                                                                </td>
-                                                                <td className='d-flex gap-5'>
-                                                                        <button className='flex-grow-1 btn btn-success' data-bs-toggle='modal' data-bs-target='#myModal' onClick={() => viewFun(value)}>View</button>
+                    {edit === index ? (
+                      <button
+                        className="flex-grow-1 btn btn-warning"
+                        onClick={updateFun}
+                      >
+                        Update
+                      </button>
+                    ) : (
+                      <button
+                        className="flex-grow-1 btn btn-warning"
+                        onClick={() => editFun(index)}
+                      >
+                        Edit
+                      </button>
+                    )}
 
-                                                                        { 
-                                                                                edit === index 
-                                                                                ? 
-                                                                                <button className='flex-grow-1 btn btn-warning' onClick={updateFun}>Update</button>
-                                                                                :
-                                                                                <button className='flex-grow-1 btn btn-warning' onClick={() => editFun(index)}>Edit</button>
-                                                                        }
+                    <button
+                      className="flex-grow-1 btn btn-danger"
+                      onClick={() => deleteFun(index)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+            
+          </table>
+        )}
 
+        <div className="modal fade" id="myModal">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h3>Your Task</h3>
+                <button className="btn-close" data-bs-dismiss="modal"></button>
+              </div>
+              <div className="modal-body">
+                <h3 className="text-center">{viewText}</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-
-                                                                        <button className='flex-grow-1 btn btn-danger' onClick={() => deleteFun(index)}>Delete</button>
-                                                                </td>
-                                                        </tr>
-                                                ))
-                                        }
-                                </tbody>
-                        </table>
-                        }
-
-                        <div className="modal fade" id='myModal'>
-                                <div className="modal-dialog">
-                                        <div className="modal-content">
-                                                <div className="modal-header">
-                                                        <h3>Your Task</h3>
-                                                        <button className='btn-close' data-bs-dismiss='modal'></button>
-                                                </div>
-                                                <div className="modal-body">
-                                                        <h3 className='text-center'>{viewText}</h3>
-                                                </div>  
-                                        </div>
-                                </div>
-                        </div>
-
-                </div>
-        </>
-        )
-}
-
-export default Crud
+export default Crud;
